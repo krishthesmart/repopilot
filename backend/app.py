04de5ea850d43404e77a64e5c26eac950b698f20
gpluselevator <gpluselevator@gmail.com>
2026-05-20 20:08:36 -0700

    Allow local Vite frontend ports

--- backend/app.py
+++ backend/app.py
@@ -96,7 +96,7 @@ store = Store()
 app = FastAPI(title="RepoPilot", version="0.1.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origin_regex=r"http://(localhost|127\.0\.0\.1):51[0-9]{2}",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
